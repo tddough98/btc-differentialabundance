0e0abb9da417680ab0e47f384f3a3b2e95bf7490
tddough98 <40068392+tddough98@users.noreply.github.com>
2024-05-21 13:49:50 -0400

    Create samplesheet.csv and contrasts.csv

--- .cirro/preprocess.py
+++ .cirro/preprocess.py
@@ -1,9 +1,36 @@
+import csv
 
-# create sample sheet based on what the pipeline needs...
-# e.g. .csv editing
+from cirro.helpers.preprocess_dataset import PreprocessDataset
 
-# for differential abundance need:
-# sample,fastq_1,fastq_2,condition,replicate,batch
+def make_samplesheet(ds):
+    # Make a wide-form samplesheet with columns: sample, fastq_1, and fastq_2
+    samplesheet = ds.wide_samplesheet()
 
-# currently have
-# sample,fastq_1,fastq_2,strandedness
+    # Merge in the metadata for these samples (if any exists)
+    samplesheet = pd.merge(samplesheet, ds.samplesheet, left_on="sample", right_on="sample")
+    variable = ds.params["variable"]
+    if variable not in samplesheet:
+        raise ValueError(f"Column {variable} not found in samplesheet")
+
+    # Save to a file
+    samplesheet.to_csv("samplesheet.csv", index=None)
+
+    # Set up a workflow param pointing to that file (e.g., for nf-core/rnaseq)
+    ds.add_param("input", "samplesheet.csv")
+
+def make_contrasts(ds):
+    # Generate the contrasts.csv
+    reference = ds.params["reference"]
+    target = ds.params["target"]
+    ds.remove_param("reference")
+    ds.remove_param("target")
+    ds.remove_param("variable")
+
+    with open("contrasts.csv", "w") as f:
+        csv.writer(f).writerow(["id", "variable", "reference", "target"])
+        csv.writer(f).writerow([f"{reference}_vs_{target}", variable, reference, target])
+
+if __name__ == "__main__":
+    ds = PreprocessDataset.from_running()
+    make_samplesheet(ds)
+    make_contrasts(ds)
