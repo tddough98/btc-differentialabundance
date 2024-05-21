629663026d91a0a72d780ce4e50e9c1f81051266
tddough98 <40068392+tddough98@users.noreply.github.com>
2024-05-21 11:41:45 -0400

    Add cirro pipeline configuration

---
+++ .cirro/preprocess.py
@@ -0,0 +1,9 @@
+
+# create sample sheet based on what the pipeline needs...
+# e.g. .csv editing
+
+# for differential abundance need:
+# sample,fastq_1,fastq_2,condition,replicate,batch
+
+# currently have
+# sample,fastq_1,fastq_2,strandedness
